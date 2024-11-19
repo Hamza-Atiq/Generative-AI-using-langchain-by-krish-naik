fa64c6517d59732f3da3345b2c72205ea57340ea
Hamza Atiq <66232332+Hamza-Atiq@users.noreply.github.com>
2024-11-19 14:03:56 -0800

    01.0_basics

---
+++ 01_Python_Basics/01.0_basics.py
@@ -0,0 +1,31 @@
+# Basic syntax rules in python
+name : str = 'hamza'
+Name : str = 'Atiq'
+
+print(name)
+print(Name)
+
+#######################################
+
+# Indentation
+
+age : int = 28
+
+if age >= 30:
+
+    print(age)
+
+print(age)
+
+################################
+
+# Line continuation
+
+total : int = 1 + 2 + 4 + 6 + 7 + 9 \
++ 3 + 5 + 8
+
+print(total)
+
+#################################
+
+
